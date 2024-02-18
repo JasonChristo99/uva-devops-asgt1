f239ddec82e7fda20ae732218791dc1d9afcf733
Jason <christofilakisiasonas@gmail.com>
2024-02-18 02:55:28 +0100

    test

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -36,14 +36,14 @@ def add(student=None):
     result = collection.insert_one(student.to_dict())
     return student.student_id
 
-# def get_by_id(student_id=None, subject=None):
-#     student = collection.find_one({"student_id": student_id}, {'_id': False})
-#     if not student:
-#         return 'not found', 404
-#     return student
-#
-# def delete(student_id=None):
-#     result = collection.delete_one({"student_id": student_id})
-#     if result.deleted_count == 0:
-#         return 'not found', 404
-#     return student_id
+def get_by_id(student_id=None, subject=None):
+    student = collection.find_one({"student_id": student_id}, {'_id': False})
+    if not student:
+        return 'not found', 404
+    return student
+
+def delete(student_id=None):
+    result = collection.delete_one({"student_id": student_id})
+    if result.deleted_count == 0:
+        return 'not found', 404
+    return student_id
