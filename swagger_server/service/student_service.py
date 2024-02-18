bf85e833218869250966f4fb045957a37a9ad7ef
Jason <christofilakisiasonas@gmail.com>
2024-02-18 02:12:11 +0100

    test

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -5,13 +5,13 @@ from pymongo import MongoClient
 
 # Connection to MongoDB
 mongo_uri = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
-print('Connecting to MongoDB at', mongo_uri)
 client = MongoClient(mongo_uri)
 db = client['students_db']
 collection = db['students']
 
 
 def add(student=None):
+    print('Connecting to MongoDB at', mongo_uri)
     # If student_id is not provided, generate a new unique id
     if not student.student_id:
         # Find the maximum student_id and increment it by 1
