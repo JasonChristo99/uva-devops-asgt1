9ba5c014f80e649d54e3a66735fbedce6093cc87
Jason <christofilakisiasonas@gmail.com>
2024-02-18 02:25:49 +0100

    test

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,8 @@ WORKDIR /usr/src/app
 
 COPY requirements.txt /usr/src/app/
 
-RUN pip3 install --no-cache-dir -r requirements.txt
+# RUN pip3 install --no-cache-dir -r requirements.txt
+RUN pip3 install -r requirements.txt
 
 COPY . /usr/src/app
 
--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -4,15 +4,15 @@ from functools import reduce
 from pymongo import MongoClient
 
 # Connection to MongoDB
-# mongo_uri = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
+mongo_uri = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
 # client = MongoClient(mongo_uri)
 # db = client['students_db']
 # collection = db['students']
 
 
 def add(student=None):
+    print('Connecting to MongoDB at', mongo_uri)
     return 'already exists', 409
-    # print('Connecting to MongoDB at', mongo_uri)
     # # If student_id is not provided, generate a new unique id
     # if not student.student_id:
     #     # Find the maximum student_id and increment it by 1
